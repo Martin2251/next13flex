5ccd1e1999b1dfb661953311a14d5d21d9df6d04
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-06-26 07:34:22 +0100

    new components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 const Home = () => {
-    return <h1>Home</h1>;
+    return (
+        <section className="flex-start flex-col paddings mb-16">
+         <h1>Categories</h1>Categories
+         <h1>Posts</h1>
+         <h1>Load More</h1>
+        </section>
+    )
 
 
 }
