3cfbbe47d6b70fa087432cd4008ef4050d41f613
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-06-26 08:03:51 +0100

    typescript

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,21 @@
 import React from 'react'
 import Image from 'next/image'
+import { footerLinks } from '@/constants'
+
+
+type ColumnProps= {
+    title:string;
+    links:Array<string>
+}
+
+const FooterColumn = ({title,links}:ColumnProps) => (
+    <div className="footer_column">
+        <h4 className='font-semibold'>Title</h4>
+        <ul className='flex flex-col gap-2 font-normal'>
+            Links
+        </ul>
+    </div>
+)
 
 const Footer = () => {
   return (
@@ -14,6 +30,10 @@ const Footer = () => {
                 
                 />
                 <p className='text-start text-sm font-normal mt-5 max-w-xs'> This is for the community for developers and creatives to share and grow and get going</p>
+            </div>
+            <div className='flex flex-wrap gap-12'>
+                <FooterColumn title={footerLinks[0].title} links={footerLinks[0].links} />
+
             </div>
         </div>
 
