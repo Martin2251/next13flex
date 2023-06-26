5eebbc26a02d15f67b7cf4260c86b140e4104d94
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-06-26 07:55:04 +0100

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='flexStart footer'>
+        <div className='flex flex-col gap-12 w-full'>
+            <div className='flex items-start flex-col'>
+                <Image  
+                src="/logo-purple.svg"
+                width={115}
+                height={38}
+                alt='flexible'
+                
+                />
+
+            </div>
+        </div>
+
+    </footer>
   )
 }
 
