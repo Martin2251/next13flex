5aacf5c14d567ca16ec219f9671cf41667f8bf23
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-06-26 07:50:57 +0100

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,11 @@
 import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
+import { NavLinks } from '@/constants'
+import AuthProviders from './AuthProviders'
 
 const Navbar = () => {
+    const session = {}
   return (
     <nav className='flexBetween navbar'>
         <div className='flex-1 flexStart gap-10'>
@@ -12,6 +15,28 @@ const Navbar = () => {
                 height={43}
                 alt="Logo"/>
             </Link>
+            <ul className='xl:flex hidden text-small gap-7'>
+                {NavLinks.map((link) =>(
+                    <Link href={link.href} key={link.key}>
+                        {link.text}
+                    </Link>
+                ))}
+
+            </ul>
+        </div>
+        <div className='flexCenter gap-4'>
+            {session ? (
+                <>
+                UserPhoto
+                <Link href="/create-project">
+                Share Work
+                
+                </Link>
+                </>
+            ):(
+                <AuthProviders  />
+            )}
+
         </div>
         </nav>
   )
